1bd67f5469fb3d87437c98325543a15b38572257
Justin Boileau <justin.boileau@ketch.com>
2024-03-21 15:48:05 -0700

    feat: update interface

--- package/src/KetchServiceProvider/KetchServiceProvider.tsx
+++ package/src/KetchServiceProvider/KetchServiceProvider.tsx
@@ -1,150 +1,190 @@
-// import React, {
-//   createContext,
-//   useContext,
-//   useRef,
-//   useState,
-//   useReducer,
-//   useCallback,
-//   useEffect,
-// } from 'react';
-// import WebView, { type WebViewMessageEvent } from 'react-native-webview';
+import React, {
+  createContext,
+  useContext,
+  useRef,
+  useState,
+  useReducer,
+  useCallback,
+  useEffect,
+} from 'react';
+import WebView, { type WebViewMessageEvent } from 'react-native-webview';
 
-// import { ShownComponent, type KetchMobile, type KetchService } from '../types';
-// import styles from './styles';
-// import { Action, initialParameters, reducer } from './reducer';
-// import { View } from 'react-native';
+import { ShownComponent, type KetchMobile, type KetchService } from '../types';
+import styles from './styles';
+import { Action, initialParameters, reducer } from './reducer';
+import { View } from 'react-native';
+import { KetchApiRegion, LogLevel } from '../enums';
 
-// interface KetchServiceProviderParams extends KetchMobile {
-//   orgPropertyNameCode: string;
-//   children: JSX.Element;
-// }
+interface KetchServiceProviderParams extends KetchMobile {
+  children: JSX.Element;
+}
 
-// type EventName =
-//   | 'consent'
-//   | 'environment'
-//   | 'geoip'
-//   | 'identities'
-//   | 'jurisdiction'
-//   | 'regionInfo'
-//   | 'willShowExperience'
-//   | 'hideExperience';
+type EventName =
+  | 'consent'
+  | 'environment'
+  | 'geoip'
+  | 'identities'
+  | 'jurisdiction'
+  | 'regionInfo'
+  | 'willShowExperience'
+  | 'hideExperience';
 
-// interface OnMessageEventData {
-//   event: string;
-//   data: EventName;
-// }
+interface OnMessageEventData {
+  event: string;
+  data: EventName;
+}
 
-// const KetchServiceContext = createContext<KetchService>({
-//   shownComponent: null,
-//   showConsent: () => {},
-//   showPreferences: () => {},
-//   hide: () => {},
-//   updateParameters: () => {},
-// });
+const KetchServiceContext = createContext<KetchService>({
+  shownComponent: null,
+  showConsent: () => {},
+  showPreferences: () => {},
+  hide: () => {},
+  updateParameters: () => {},
+});
 
-// export const KetchServiceProvider = ({
-//   organization,
-//   orgPropertyNameCode,
-//   children,
-// }: KetchServiceProviderParams) => {
-//   const webViewRef = useRef<WebView>(null);
-//   const source = require('../index.html');
+export const KetchServiceProvider: React.FC<KetchServiceProviderParams> = ({
+  organizationCode,
+  propertyCode,
+  identities,
+  languageCode, // TODO: Language should default to device language
+  regionCode,
+  jurisdictionCode = 'default',
+  environmentName,
+  ketchApiRegion = KetchApiRegion.prdUS,
+  logLevel = LogLevel.ERROR,
+  forceConsentExperience = false,
+  forcePreferenceExperience = false,
+  hideExperience = false,
+  onEnvironmentUpdated,
+  onRegionUpdated,
+  onJurisdictionUpdated,
+  onIdentitiesUpdated,
+  onConsentUpdated,
+  onError,
+  onPrivacyStringUpdated,
+  children,
+}) => {
+  // Temporarily log all params for debugging and to supress lint warnings
+  console.log('Params: ', {
+    organizationCode,
+    propertyCode,
+    identities,
+    languageCode,
+    regionCode,
+    jurisdictionCode,
+    environmentName,
+    ketchApiRegion,
+    logLevel,
+    forceConsentExperience,
+    forcePreferenceExperience,
+    hideExperience,
+    onEnvironmentUpdated,
+    onRegionUpdated,
+    onJurisdictionUpdated,
+    onIdentitiesUpdated,
+    onConsentUpdated,
+    onError,
+    onPrivacyStringUpdated,
+  });
 
-//   const [shownComponent, setShownComponent] = useState<ShownComponent | null>(
-//     null
-//   );
-//   const [isVisible, setIsVisible] = useState(false);
+  const webViewRef = useRef<WebView>(null);
+  const source = require('../index.html');
 
-//   const [parameters, dispatch] = useReducer(reducer, {
-//     ...initialParameters,
-//     organization,
-//     orgPropertyNameCode,
-//   });
+  const [shownComponent, setShownComponent] = useState<ShownComponent | null>(
+    null
+  );
+  const [isVisible, setIsVisible] = useState(false);
 
-//   useEffect(() => {
-//     if (shownComponent === ShownComponent.CONSENT) {
-//       webViewRef.current?.injectJavaScript(`ketch('showConsent')`);
-//     }
+  const [parameters, dispatch] = useReducer(reducer, {
+    ...initialParameters,
+    organizationCode,
+    propertyCode,
+  });
 
-//     if (shownComponent === ShownComponent.PREFERENCES) {
-//       webViewRef.current?.injectJavaScript(`ketch('showPreferences')`);
-//     }
+  useEffect(() => {
+    if (shownComponent === ShownComponent.CONSENT) {
+      webViewRef.current?.injectJavaScript(`ketch('showConsent')`);
+    }
 
-//     if (shownComponent === null) {
-//       setIsVisible(false);
-//     }
-//   }, [shownComponent]);
+    if (shownComponent === ShownComponent.PREFERENCES) {
+      webViewRef.current?.injectJavaScript(`ketch('showPreferences')`);
+    }
 
-//   const showConsent = useCallback(() => {
-//     setShownComponent(ShownComponent.CONSENT);
-//   }, []);
+    if (shownComponent === null) {
+      setIsVisible(false);
+    }
+  }, [shownComponent]);
 
-//   const showPreferences = useCallback(() => {
-//     setShownComponent(ShownComponent.PREFERENCES);
-//   }, []);
+  const showConsent = useCallback(() => {
+    setShownComponent(ShownComponent.CONSENT);
+  }, []);
 
-//   const hide = useCallback(() => {
-//     setShownComponent(null);
-//   }, []);
+  const showPreferences = useCallback(() => {
+    setShownComponent(ShownComponent.PREFERENCES);
+  }, []);
 
-//   const updateParameters = (params: Partial<KetchMobile>) => {
-//     dispatch({ type: Action.UPDATE_PARAMETERS, payload: params });
-//   };
+  const hide = useCallback(() => {
+    setShownComponent(null);
+  }, []);
 
-//   const handleMessageRecieve = (e: WebViewMessageEvent) => {
-//     const data = JSON.parse(e.nativeEvent.data) as OnMessageEventData;
-//     console.log('onMessage', data);
-//     switch (data.event) {
-//       case 'willShowExperience':
-//         setIsVisible(true);
-//         break;
+  const updateParameters = (params: Partial<KetchMobile>) => {
+    dispatch({ type: Action.UPDATE_PARAMETERS, payload: params });
+  };
 
-//       case 'hideExperience':
-//       case 'experienceClosed':
-//       case 'tapOutside':
-//         setIsVisible(false);
-//         break;
+  const handleMessageRecieve = (e: WebViewMessageEvent) => {
+    const data = JSON.parse(e.nativeEvent.data) as OnMessageEventData;
+    console.log('onMessage', data);
+    switch (data.event) {
+      case 'willShowExperience':
+        setIsVisible(true);
+        break;
 
-//       default:
-//         break;
-//     }
-//   };
+      case 'hideExperience':
+      case 'experienceClosed':
+      case 'tapOutside':
+        setIsVisible(false);
+        break;
 
-//   return (
-//     <KetchServiceContext.Provider
-//       value={{
-//         shownComponent,
-//         showConsent,
-//         showPreferences,
-//         hide,
-//         updateParameters,
-//       }}
-//     >
-//       {children}
+      default:
+        break;
+    }
+  };
 
-//       <View
-//         style={[styles.container, isVisible ? styles.shown : styles.hidden]}
-//       >
-//         <WebView
-//           ref={webViewRef}
-//           source={source}
-//           javaScriptEnabled
-//           webviewDebuggingEnabled
-//           domStorageEnabled
-//           injectedJavaScriptObject={{
-//             orgCode: parameters.organization,
-//             orgPropertyNameCode: parameters.orgPropertyNameCode,
-//           }}
-//           onMessage={handleMessageRecieve}
-//           style={styles.webView}
-//         />
-//       </View>
-//     </KetchServiceContext.Provider>
-//   );
-// };
+  return (
+    <KetchServiceContext.Provider
+      value={{
+        shownComponent,
+        showConsent,
+        showPreferences,
+        hide,
+        updateParameters,
+      }}
+    >
+      {children}
 
-// export const useKetchService = () => {
-//   const context = useContext(KetchServiceContext);
+      <View
+        style={[styles.container, isVisible ? styles.shown : styles.hidden]}
+      >
+        <WebView
+          ref={webViewRef}
+          source={source}
+          javaScriptEnabled
+          webviewDebuggingEnabled
+          domStorageEnabled
+          injectedJavaScriptObject={{
+            orgCode: parameters.organizationCode,
+            orgPropertyNameCode: parameters.propertyCode,
+          }}
+          onMessage={handleMessageRecieve}
+          style={styles.webView}
+        />
+      </View>
+    </KetchServiceContext.Provider>
+  );
+};
 
-//   return context ? context : ({} as KetchService);
-// };
+export const useKetchService = () => {
+  const context = useContext(KetchServiceContext);
+
+  return context ? context : ({} as KetchService);
+};
--- package/src/types/index.ts
+++ package/src/types/index.ts
@@ -1,27 +1,123 @@
-import type { KetchApiRegion } from '../enums';
+import type { KetchApiRegion, LogLevel } from '../enums';
+
+/**
+ * Consent object
+ * @field purposes - mapping of purpose codes to the consent value (yes or no)
+ * @field vendors - list of TCF vendor codes
+ * @field protocols - mapping of privacy protocols to purpose codes
+ */
+export interface Consent {
+  purposes?: Record<string, boolean>;
+  vendors?: string[];
+  protocols?: Record<string, string>;
+}
 
 export interface KetchMobile {
-  organization: string | undefined;
+  /**
+   * Ketch organization code
+   */
+  organizationCode: string;
+
+  /**
+   * Ketch property code
+   */
+  propertyCode: string;
+
+  /**
+   * Ketch identity map of identity space names to values
+   */
   identities?: Record<string, string>;
-  language?: string;
-  region?: string;
-  jurisdiction?: string;
-  environment?: string;
+
+  /**
+   * ISO 639-1 language code
+   */
+  languageCode?: string;
+
+  /**
+   * ISO 3166 Country code
+   */
+  regionCode?: string;
+
+  /**
+   * Ketch jurisdiction code
+   */
+  jurisdictionCode?: string;
+
+  /**
+   * Ketch environment name
+   */
+  environmentName?: string;
+
+  /**
+   * Ketch API region
+   */
   ketchApiRegion?: KetchApiRegion;
-  logLevel?: string;
-  onEnvironmentUpdated?: (environment: string) => void;
-  onRegionUpdated?: (region: string) => void;
-  onJurisdictionUpdated?: (jurisdiction: string) => void;
+
+  /**
+   * Log level for SDK log messages
+   */
+  logLevel?: LogLevel;
+
+  /**
+   * Force show the consent experience
+   */
+  forceConsentExperience?: boolean;
+
+  /**
+   * Force show the preference experience
+   */
+  forcePreferenceExperience?: boolean;
+
+  /**
+   * Do not display any experiences, forced or otherwise.
+   */
+  hideExperience?: boolean;
+
+  /**
+   * Environment update listener
+   * @param environment The new environment name
+   */
+  onEnvironmentUpdated?: (environmentName: string) => void;
+
+  /**
+   * Region update listener
+   * @param region The updated ISO 3166 region code
+   */
+  onRegionUpdated?: (regionCode: string) => void;
+
+  /**
+   * Jurisdiction update listener
+   * @param jurisdiction The updated jurisdiction code
+   */
+  onJurisdictionUpdated?: (jurisdictionCode: string) => void;
+
+  /**
+   * Identities update listener
+   * @param identities The updated identities object
+   */
   onIdentitiesUpdated?: (identities: Record<string, string>) => void;
-  onConsentUpdated?: () => void;
-  onError?: () => void;
+
+  /**
+   * Consent update listener
+   * @param consent The updated consent object
+   */
+  onConsentUpdated?: (consent: Consent) => void;
+
+  /**
+   * Error listener
+   * @param errorMessage The error message string
+   */
+  onError?: (errorMessage: string) => void;
+
+  /**
+   * Privacy string update listener
+   * @param privacyStringKey The privacy string that was updated
+   * @param privacyStringValue The new value of the updated privacy string
+   */
   onPrivacyStringUpdated?: (
     privacyStringKey: string,
     privacyStringValue: string
   ) => void;
-
-  // ?
-  orgPropertyNameCode: string;
 }
 
 export enum ShownComponent {
