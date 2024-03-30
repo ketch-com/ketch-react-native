ecb3e828da6a5cbfab41c8b3e184bda0ab663c82
Joseph Chereshnovsky <v-joseph.chereshnovsky@ketch.com>
2024-03-30 15:36:39 +0100

    Static assets serving in Android

---
+++ package/react-native.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  dependencies: {
+    // Help rn-cli find and autolink this library
+    '@ketch-com/ketch-react-native': {
+      root: __dirname,
+    },
+  },
+  dependency: {
+    assets: ['./src/assets/'],
+  },
+};
