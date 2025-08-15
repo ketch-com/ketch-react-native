ca68a6f9fc4cfce71d1bba60b5916ec38ee28518
Justin Boileau <45305362+jboileau99@users.noreply.github.com>
2025-08-14 19:35:14 -0700

    feat: turbo module and new architecture support (#81)

--- example/App.tsx
+++ example/App.tsx
@@ -14,9 +14,8 @@ function App(): React.JSX.Element {
   return (
     <KetchServiceProvider
       organizationCode="ketch_samples"
-      propertyCode="react_native"
-      identities={{email: 'test@test.com'}}
-      autoLoad={false}>
+      propertyCode="react_native_sample_app"
+      identities={{email: 'test-123-1@gmail.com'}}>
       {/* Main.tsx is the content of your app. */}
       <Main />
     </KetchServiceProvider>
